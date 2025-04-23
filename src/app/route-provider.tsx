004346d3a9b24add00dc2e8d2778cca053809c1b
DanyaCreator <danildb2109@gmail.com>
2025-04-24 01:24:47 +0300

    feat: add router

---
+++ src/app/route-provider.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { ReactNode } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useAuth } from '@/hooks/use-auth.ts';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Home } from '@/pages/home/home.tsx';
+import { RouterPaths } from '@/app/consts/RouterPaths.ts';
+
+export const RouteProvider = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={RouterPaths.HOME} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const layoutTransition = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+  transition: { duration: 0.5 },
+};
+
+function AnimatedRoute({ children }: { children: ReactNode }) {
+  return (
+    <AnimatePresence mode={'wait'}>
+      <motion.div {...layoutTransition}>{children}</motion.div>
+    </AnimatePresence>
+  );
+}
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const { user, loading } = useAuth();
+
+  // TODO: LOADING COMPONENT
+  if (loading) return <div>Loading...</div>;
+  if (!user) return <Navigate to='/login' />;
+
+  return children;
+}
