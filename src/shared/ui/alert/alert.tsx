065f09a1a142a5bc2cec6cab25f3db4828e30bcf
mihaj <mihajlow.dima258@gmail.com>
2025-04-25 23:42:57 +0300

    feat: add toast shared component

--- src/shared/ui/alert/alert.tsx
+++ src/shared/ui/alert/alert.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react';
 
+import { Toaster as Sonner } from 'sonner';
+
 import clsx from 'clsx';
 
 import styles from './alert.module.scss';
 
-type ButtonDesign = 'default' | 'destructive';
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  return (
+    <Sonner
+      toastOptions={{
+        duration: 300000,
+      }}
+      {...props}
+    />
+  );
+};
+
+type AlertDesign = 'default' | 'success' | 'error';
 
 interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
-  design?: ButtonDesign;
+  design?: AlertDesign;
 }
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
@@ -27,7 +42,11 @@ const AlertTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h5 ref={ref} className={clsx(styles.alertTitle, className)} {...props} />
+  <h5
+    ref={ref}
+    className={clsx(styles['__alert-title'], className)}
+    {...props}
+  />
 ));
 AlertTitle.displayName = 'AlertTitle';
 
@@ -37,10 +56,15 @@ const AlertDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={clsx(styles.alertDescription, className)}
+    className={clsx(styles['__alert-description'], className)}
     {...props}
   />
 ));
 AlertDescription.displayName = 'AlertDescription';
 
-export { Alert, AlertTitle, AlertDescription };
+export {
+  Alert as UiAlert,
+  AlertTitle as UiAlertTitle,
+  AlertDescription as UiAlertDescription,
+  Toaster,
+};
