57423852da7aaedc76fcd0f95a9a313b05f0bb85
DanyaCreator <danildb2109@gmail.com>
2025-05-02 09:51:29 +0300

    feat: base redux configuration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import { RouteProvider } from './route-provider';
 import { Toaster } from '@/shared/ui/toast/toast';
+import { Provider } from 'react-redux';
+import { store } from '@/shared/lib/redux/store.ts';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Toaster />
       <RouteProvider />
-    </>
+    </Provider>
   );
 }
 
