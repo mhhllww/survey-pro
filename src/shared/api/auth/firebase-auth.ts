466f72aa4a8c5fcf3dedd75dc9a839575e14afcb
mihaj <mihajlow.dima258@gmail.com>
2025-05-13 12:14:06 +0300

    feat: reset password && sign out && firestorage config

--- src/shared/api/auth/firebase-auth.ts
+++ src/shared/api/auth/firebase-auth.ts
@@ -4,7 +4,11 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
+  reauthenticateWithCredential,
   GithubAuthProvider,
+  EmailAuthProvider,
+  updatePassword,
+  AuthError,
 } from 'firebase/auth';
 
 import { auth } from '@/shared/lib/firebase/firebase.ts';
@@ -23,6 +27,45 @@ export const loginWithGoogle = () => {
 
 export const loginWithGithub = () => {
   return signInWithPopup(auth, new GithubAuthProvider());
+};
+
+export const resetPassword = async (
+  currentPassword: string,
+  newPassword: string
+) => {
+  const user = auth.currentUser;
+
+  // FIXME: Будем делать норм систему уведомлений??
+  if (!user) {
+    return;
+  }
+  if (currentPassword !== newPassword) {
+    return;
+  }
+
+  if (user && user.email) {
+    const credential = EmailAuthProvider.credential(
+      user.email,
+      currentPassword
+    );
+
+    try {
+      await reauthenticateWithCredential(user, credential);
+      await updatePassword(user, newPassword);
+    } catch (error: unknown) {
+      const authError = error as AuthError;
+
+      // FIXME: Будем делать норм систему уведомлений??
+      switch (authError.code) {
+        case 'auth/wrong-password':
+          break;
+        case 'auth/requires-recent-login':
+          break;
+        default:
+          console.error(authError.message);
+      }
+    }
+  }
 };
 
 export const signOutUser = () => {
