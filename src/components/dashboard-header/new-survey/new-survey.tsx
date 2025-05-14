7d57f3d3b6c0862d7fddd2a1924bf6b5a08f2602
Eduard <eduard.agishev.2005@mail.ru>
2025-05-14 21:14:50 +0300

    feat: add sign out and reset password

--- src/components/dashboard-header/new-survey/new-survey.tsx
+++ src/components/dashboard-header/new-survey/new-survey.tsx
@@ -1,23 +1,12 @@
 import {
   UiDialog,
   UiDialogContent,
-  UiDialogDescription,
   UiDialogFooter,
   UiDialogHeader,
-  UiDialogTitle,
   UiDialogTriggerProps,
 } from '@/shared/ui/dialog/dialog.tsx';
 import { UiButton } from '@/shared/ui/button/button.tsx';
-import { UiLabel } from '@/shared/ui/label/label.tsx';
-import { UiInput } from '@/shared/ui/input/input.tsx';
 import { CirclePlusIcon } from 'lucide-react';
-import {
-  UiDropdownMenu,
-  UiDropdownMenuGroup,
-  UiDropdownMenuItem,
-  UiDropdownMenuShortcut,
-} from '@/shared/ui/dropdown-menu/dropdown-menu.tsx';
-import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 
 interface NewSurveyProps {
   buttonText?: string;
@@ -32,53 +21,8 @@ export const NewSurvey = ({ buttonText = 'New Survey' }: NewSurveyProps) => {
         </UiButton>
       </UiDialogTriggerProps>
       <UiDialogContent>
-        <UiDialogHeader>
-          <UiDialogTitle>Create New Survey</UiDialogTitle>
-          <UiDialogDescription>
-            Enter the basic information for your survey. You can add questions
-            in the next step.
-          </UiDialogDescription>
-        </UiDialogHeader>
-        <div>
-          <div>
-            <UiLabel htmlFor='text'>Survey Title</UiLabel>
-            <UiInput type='text' required />
-          </div>
-          <div>
-            <UiLabel htmlFor='text'>Description</UiLabel>
-            <UiInput type='text' required />
-          </div>
-          <div>
-            <UiLabel htmlFor='text'>Category</UiLabel>
-            <UiDropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <UiLabel>Market Research</UiLabel>
-              </DropdownMenuTrigger>
-              <UiDropdownMenuGroup>
-                <UiDropdownMenuItem>
-                  Profile
-                  <UiDropdownMenuShortcut>⇧⌘P</UiDropdownMenuShortcut>
-                </UiDropdownMenuItem>
-                <UiDropdownMenuItem>
-                  Billing
-                  <UiDropdownMenuShortcut>⌘B</UiDropdownMenuShortcut>
-                </UiDropdownMenuItem>
-                <UiDropdownMenuItem>
-                  Settings
-                  <UiDropdownMenuShortcut>⌘S</UiDropdownMenuShortcut>
-                </UiDropdownMenuItem>
-                <UiDropdownMenuItem>
-                  Keyboard shortcuts
-                  <UiDropdownMenuShortcut>⌘K</UiDropdownMenuShortcut>
-                </UiDropdownMenuItem>
-              </UiDropdownMenuGroup>
-            </UiDropdownMenu>
-          </div>
-        </div>
-        <UiDialogFooter>
-          <UiButton design='outline'>Cancel</UiButton>
-          <UiButton>Continue</UiButton>
-        </UiDialogFooter>
+        <UiDialogHeader></UiDialogHeader>
+        <UiDialogFooter></UiDialogFooter>
       </UiDialogContent>
     </UiDialog>
   );
--- src/components/dashboard-header/settings/settings.tsx
+++ src/components/dashboard-header/settings/settings.tsx
@@ -11,8 +11,27 @@ import { LockIcon, LogOutIcon, SettingsIcon } from 'lucide-react';
 import { UiLabel } from '@/shared/ui/label/label.tsx';
 import { UiInput } from '@/shared/ui/input/input.tsx';
 import '../settings/settings.scss';
+import { resetPassword, signOutUser } from '@/shared/api/auth/firebase-auth.ts';
+import { useState } from 'react';
 
 export const Settings = () => {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handlePasswordUpdate = async () => {
+    if (newPassword !== confirmPassword) return;
+
+    try {
+      await resetPassword(currentPassword, newPassword);
+      setCurrentPassword('');
+      setNewPassword('');
+      setConfirmPassword('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <UiSheet>
       <UiSheetTrigger asChild>
@@ -35,19 +54,36 @@ export const Settings = () => {
           <div className='sheet-container'>
             <div className='sheet-fields'>
               <UiLabel htmlFor='password'>Current Password</UiLabel>
-              <UiInput type='password' required />
+              <UiInput
+                type='password'
+                required
+                value={currentPassword}
+                onChange={(e) => setCurrentPassword(e.target.value)}
+              />
             </div>
             <div className='sheet-fields'>
               <UiLabel htmlFor='password'>New Password</UiLabel>
-              <UiInput type='password' required />
+              <UiInput
+                type='password'
+                required
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
             </div>
             <div className='sheet-fields'>
               <UiLabel htmlFor='password'>Confirm New Password</UiLabel>
-              <UiInput type='password' required />
+              <UiInput
+                type='password'
+                required
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
             </div>
           </div>
-          <UiButton size='lg'>Update Password</UiButton>
-          <UiButton design='outline' size='lg'>
+          <UiButton size='lg' onClick={handlePasswordUpdate}>
+            Update Password
+          </UiButton>
+          <UiButton design='outline' size='lg' onClick={signOutUser}>
             <LogOutIcon /> Sign Out
           </UiButton>
         </div>
