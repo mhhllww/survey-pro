1a20df38b61e4337d93bb8f51271a569709332ec
DanyaCreator <danildb2109@gmail.com>
2025-05-16 00:39:18 +0300

    fix: adaptive & interactive btns

--- src/components/contact-section/contact-section.tsx
+++ src/components/contact-section/contact-section.tsx
@@ -1,11 +1,13 @@
 import { UiButton } from '@/shared/ui/button/button.tsx';
 import { ArrowRightIcon } from 'lucide-react';
 import '../contact-section/contact_section.scss';
+import { Link } from 'react-router-dom';
+import { RouterPaths } from '@/app/consts/RouterPaths.ts';
 
 export const ContactSection = () => {
   return (
-    <section id='contact' className='cta'>
-      <div className='cta__content'>
+    <section id='contact' className='cta-container'>
+      <div className='cta'>
         <h2 className='cta__title'>
           Ready to Start Collecting Valuable Insights?
         </h2>
@@ -15,11 +17,12 @@ export const ContactSection = () => {
         </p>
 
         <div className='cta__buttons'>
-          <UiButton size='lg'>
-            Get Started for Free
-            <ArrowRightIcon />
+          <UiButton size='lg' asChild>
+            <Link to={RouterPaths.DASHBOARD}>
+              Get Started for Free
+              <ArrowRightIcon />
+            </Link>
           </UiButton>
-
           <UiButton design='outline' size='lg'>
             Contact Sales
           </UiButton>
--- src/components/hero-section/hero_section.tsx
+++ src/components/hero-section/hero_section.tsx
@@ -3,6 +3,7 @@ import '../hero-section/hero_section.scss';
 import HeroPhoto from '../../assets/HeroPhoto.jpeg';
 import { ArrowRightIcon } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { RouterPaths } from '@/app/consts/RouterPaths.ts';
 
 export const HeroSection = () => {
   return (
@@ -11,22 +12,18 @@ export const HeroSection = () => {
         <h1 className='hero-content__headline'>
           Create, Collect, and Analyze Survey Data with Ease
         </h1>
-        <p className='hero-content__text text-caption'>
+        <p className='hero-content__text'>
           SurveyPro helps you gather insights, make data-driven decisions, and
           improve your products and services with powerful survey tools.
         </p>
-        <UiButton className='hero-black_button' size='lg'>
-          <Link to='/register'>
-            <div className='hero-content__button'>
-              Get Started
-              <ArrowRightIcon />
-            </div>
+        <UiButton size='lg' asChild className='hero-content__btn'>
+          <Link to={RouterPaths.DASHBOARD}>
+            Get Started
+            <ArrowRightIcon />
           </Link>
         </UiButton>
       </div>
-      <div className='hero-image'>
-        <img src={HeroPhoto} alt='Survey' className='hero-image__image' />
-      </div>
+      <img src={HeroPhoto} alt='Survey' className='hero-image' />
     </section>
   );
 };
