4c8f0962cf9f59fff6f6025798b90f1575a44efa
DanyaCreator <danildb2109@gmail.com>
2025-05-16 22:01:21 +0300

    feat: scroll area for analytics page

--- src/pages/analytics/analytics.tsx
+++ src/pages/analytics/analytics.tsx
@@ -35,6 +35,7 @@ import {
   fullSurveyData,
   FullSurveyData,
 } from '@/pages/analytics/full-survey-data.ts';
+import { UiScrollArea } from '@/shared/ui/scroll-area/scroll-area.tsx';
 
 const surveys = [
   'Graph with first survey',
@@ -85,106 +86,109 @@ export const Analytics = () => {
   const [selected, setSelected] = useState<string>(surveys[0]);
 
   return (
-    <div className={'analytics'}>
-      <div className={'analytics-content'}>
-        <header className={'analytics-content__header'}>
-          <div className={'analytics-content__header-text'}>
-            <h2>Analytics</h2>
-            <span className={'analytics-content__subtitle'}>
-              View and analyze your survey responses
-            </span>
+    <UiScrollArea>
+      <div className={'analytics'}>
+        <div className={'analytics-content'}>
+          <header className={'analytics-content__header'}>
+            <div className={'analytics-content__header-text'}>
+              <h2>Analytics</h2>
+              <span className={'analytics-content__subtitle'}>
+                View and analyze your survey responses
+              </span>
+            </div>
+
+            <UiDropdownMenu>
+              <UiDropdownMenuTrigger
+                asChild
+                className={'analytics-content__UiDropdownMenuTrigger'}>
+                <button
+                  className={'analytics-content__header-dropdown-trigger'}>
+                  <span>{selected}</span>
+                  <ChevronDownIcon size={16} />
+                </button>
+              </UiDropdownMenuTrigger>
+              <UiDropdownMenuContent
+                sideOffset={5}
+                className={'analytics-content__UiDropdownMenuContent'}>
+                {surveys.map((survey) => (
+                  <UiDropdownMenuCheckboxItem
+                    className={'analytics-content__UiDropdownMenuCheckboxItem'}
+                    onSelect={() =>
+                      console.log('на данный момент без функционала')
+                    }
+                    key={survey}
+                    checked={survey === selected}
+                    onCheckedChange={(checked) => {
+                      if (checked) setSelected(survey);
+                    }}>
+                    <div>{survey}</div>
+                  </UiDropdownMenuCheckboxItem>
+                ))}
+              </UiDropdownMenuContent>
+            </UiDropdownMenu>
+          </header>
+          <section className={'analytics-content__cards'}>
+            <AnalyticsCard
+              title={'Total Responses'}
+              icon={<UsersIcon size={16} />}
+              value={'1,284'}
+              change={{
+                direction: 'up',
+                percent: '12.5',
+                caption: 'from last month',
+              }}
+            />
+            <AnalyticsCard
+              title={'Completion Rate'}
+              icon={<CircleCheckBigIcon size={16} />}
+              value={'86%'}
+              change={{
+                direction: 'up',
+                percent: '2.3',
+                caption: 'from last month',
+              }}
+            />
+            <AnalyticsCard
+              title={'Avg. Time to Complete'}
+              icon={<ClockIcon size={16} />}
+              value={'4m 12s'}
+              change={{
+                direction: 'down',
+                percent: '18',
+                caption: 'from last month',
+              }}
+            />
+            <AnalyticsCard
+              title={'Satisfaction Score'}
+              icon={<UsersIcon size={16} />}
+              value={'4.2/5'}
+              change={{
+                direction: 'up',
+                percent: '0.3',
+                caption: 'from last month',
+              }}
+            />
+          </section>
+          <div>
+            <UiTabs defaultValue='overview'>
+              <UiTabsList aria-label='Пример вкладок'>
+                <UiTabsTrigger value='overview'>Overview</UiTabsTrigger>
+                <UiTabsTrigger value='questions'>Questions</UiTabsTrigger>
+              </UiTabsList>
+
+              <UiTabsContent value='overview'>
+                <AnalyticsChartCard
+                  data={getChartData(selected, fullSurveyData)}
+                />
+              </UiTabsContent>
+
+              <UiTabsContent value='questions'>
+                <QuestionTable surveyName={selected} />
+              </UiTabsContent>
+            </UiTabs>
           </div>
-
-          <UiDropdownMenu>
-            <UiDropdownMenuTrigger
-              asChild
-              className={'analytics-content__UiDropdownMenuTrigger'}>
-              <button className={'analytics-content__header-dropdown-trigger'}>
-                <span>{selected}</span>
-                <ChevronDownIcon size={16} />
-              </button>
-            </UiDropdownMenuTrigger>
-            <UiDropdownMenuContent
-              sideOffset={5}
-              className={'analytics-content__UiDropdownMenuContent'}>
-              {surveys.map((survey) => (
-                <UiDropdownMenuCheckboxItem
-                  className={'analytics-content__UiDropdownMenuCheckboxItem'}
-                  onSelect={() =>
-                    console.log('на данный момент без функционала')
-                  }
-                  key={survey}
-                  checked={survey === selected}
-                  onCheckedChange={(checked) => {
-                    if (checked) setSelected(survey);
-                  }}>
-                  <div>{survey}</div>
-                </UiDropdownMenuCheckboxItem>
-              ))}
-            </UiDropdownMenuContent>
-          </UiDropdownMenu>
-        </header>
-        <section className={'analytics-content__cards'}>
-          <AnalyticsCard
-            title={'Total Responses'}
-            icon={<UsersIcon size={16} />}
-            value={'1,284'}
-            change={{
-              direction: 'up',
-              percent: '12.5',
-              caption: 'from last month',
-            }}
-          />
-          <AnalyticsCard
-            title={'Completion Rate'}
-            icon={<CircleCheckBigIcon size={16} />}
-            value={'86%'}
-            change={{
-              direction: 'up',
-              percent: '2.3',
-              caption: 'from last month',
-            }}
-          />
-          <AnalyticsCard
-            title={'Avg. Time to Complete'}
-            icon={<ClockIcon size={16} />}
-            value={'4m 12s'}
-            change={{
-              direction: 'down',
-              percent: '18',
-              caption: 'from last month',
-            }}
-          />
-          <AnalyticsCard
-            title={'Satisfaction Score'}
-            icon={<UsersIcon size={16} />}
-            value={'4.2/5'}
-            change={{
-              direction: 'up',
-              percent: '0.3',
-              caption: 'from last month',
-            }}
-          />
-        </section>
-        <div>
-          <UiTabs defaultValue='overview'>
-            <UiTabsList aria-label='Пример вкладок'>
-              <UiTabsTrigger value='overview'>Overview</UiTabsTrigger>
-              <UiTabsTrigger value='questions'>Questions</UiTabsTrigger>
-            </UiTabsList>
-
-            <UiTabsContent value='overview'>
-              <AnalyticsChartCard
-                data={getChartData(selected, fullSurveyData)}
-              />
-            </UiTabsContent>
-
-            <UiTabsContent value='questions'>
-              <QuestionTable surveyName={selected} />
-            </UiTabsContent>
-          </UiTabs>
         </div>
       </div>
-    </div>
+    </UiScrollArea>
   );
 };
