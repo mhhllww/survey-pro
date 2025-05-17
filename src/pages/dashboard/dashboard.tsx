e179861cda2893880a512ac5f407b1a7aafcf159
Eduard <eduard.agishev.2005@mail.ru>
2025-05-17 22:55:46 +0300

    fix pr issues

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,14 +1,11 @@
 import { DashBoardHeader } from '@/components/dashboard-header/dashboard-header.tsx';
 import { MainSection } from '@/components/main-section/main-section.tsx';
-import { UiScrollArea } from '@/shared/ui/scroll-area/scroll-area.tsx';
 
 export const Dashboard = () => {
   return (
     <>
-      <UiScrollArea>
-        <DashBoardHeader />
-        <MainSection />
-      </UiScrollArea>
+      <DashBoardHeader />
+      <MainSection />
     </>
   );
 };
