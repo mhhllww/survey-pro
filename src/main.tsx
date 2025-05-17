88ac268b3ca3b4d9648debc4ece5fccca9fe5adc
Kit <nikita.khaziev@mail.ru>
2025-05-17 18:07:17 +0300

    fix: amendments were made to the PR

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from '@/app/App.tsx';
-import '@/scss/main.css';
+import '@/scss/main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
